e1a87d0697d417dbe0af2519f02edca3271fc3d2
Eun Ji <ejdd9630@gmail.com>
2022-03-24 22:44:37 +0900

    스프링을 이용하여 비밀번호 암호화 적용하기 1일차

---
+++ src/main/java/spring/Member.java
@@ -0,0 +1,28 @@
+package spring;
+
+public class Member {
+	private String id;
+	private String password;
+	private String nickname;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+}
